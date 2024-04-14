ca083420d436d7b8aa333fdc7d71424a65d0dd1c
gvrlv <d.gvrlv@gmail.com>
2024-04-14 21:06:30 +0300

    add installation via ansible playbooks

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
-### Run instruction
+# WireGuard easy install
 
-- Set the environment variable `$WG_PASSWORD` with admin wg password
-- Add execution permissions to the install file. `chmod +x install.sh`
-- Run `install.sh`
+Ansible playbooks to quickly setup a WireGuard via python `bootstrap.py` script.
+Playbooks are designed to be run on a fresh install of Ubuntu.
+The playbooks will update the system, install Docker, and then deploy the Docker WireGuard container.
+
+# Requirements
+
+- Python 3.x
+
+# Bootstrap script
+
+To use the `bootstrap.py` script, simply run it with the required command-line arguments:
+
+```shell
+python3 bootstrap.py \
+    --server-ip <server IP address> \
+    --ssh-key-path <SSH key path> \
+    --wg-password <WireGuard password> \
+    --username <username> \
+    --data-dir <data directory> \
+    --tcp-port <TCP port> \
+    --udp-port <UDP port>
+```
+
+- `--username`: The username to use when connecting to the server via SSH. The default value is `root`.
+- `--server-ip`: The IP address of the server to connect to via SSH. This argument is required.
+- `--ssh-key-path`: The path to the SSH key to use when connecting to the server. This argument is required.
+- `--data-dir`: The directory where data will be stored. The default value is `data`.
+- `--wg-password`: The password to use for the WireGuard configuration. This argument is required.
+- `--tcp-port`: The TCP port to use for the WireGuard connection. The default value is `80`.
+- `--udp-port`: The UDP port to use for the WireGuard connection. The default value is `51820`.
---
+++ bootstrap.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+
+
+def run_command(command):
+    """
+    Run a command using subprocess.run and raise an exception
+     if the command fails.
+
+    Args:
+        command (list): List containing the command and its arguments.
+    """
+    try:
+        subprocess.run(command, check=True)
+    except Exception as err:
+        raise ValueError(f'Can\'t run command: {err}')
+
+
+def run_ansible_playbook(variables):
+    """
+    Run an Ansible playbook with the specified variables.
+
+    Args:
+        variables (str): String containing the variables to pass to
+         the Ansible playbook.
+    """
+    command = [
+        'ansible-playbook',
+        'main.yml',
+        '--extra-vars',
+        f'{variables}',
+    ]
+    run_command(command)
+
+
+def set_variables(
+    username,
+    wg_server_ip,
+    wg_password,
+    data_dir,
+    tcp_port,
+    udp_port,
+    ssh_key_path,
+):
+    """
+    Set variables for the Ansible playbook.
+
+    Args:
+        username (str): Username for the playbook.
+        wg_server_ip (str): Server IP address for the playbook.
+        wg_password (str): WireGuard password for the playbook.
+        data_dir (str): Data directory for the playbook.
+        tcp_port (int): TCP port for the playbook.
+        udp_port (int): UDP port for the playbook.
+        ssh_key_path (str): SSH key path for the playbook.
+
+    Returns:
+        str: String containing the formatted variables.
+    """
+    variables = {
+        'USERNAME': username,
+        'WG_SERVER_IP_ADDRESS': wg_server_ip,
+        'WG_PASSWORD': wg_password,
+        'DATA_DIR': data_dir,
+        'WG_WEB_TCP_PORT': tcp_port,
+        'WG_WEB_UDP_PORT': udp_port,
+        'WG_SERVER_SSH_KEY_PATH': ssh_key_path,
+    }
+
+    return ' '.join(f"{key}='{value}'" for key, value in variables.items())
+
+
+def parse_args():
+    """
+    Parse command-line arguments using argparse.
+
+    Returns:
+        argparse.Namespace: Parsed arguments.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--username', type=str, default='root')
+    parser.add_argument('--server-ip', type=str, required=True)
+    parser.add_argument('--ssh-key-path', type=str, required=True)
+    parser.add_argument('--data-dir', type=str, default='data')
+    parser.add_argument('--wg-password', type=str, required=True)
+    parser.add_argument('--tcp-port', type=int, default=80)
+    parser.add_argument('--udp-port', type=int, default=51820)
+    return parser.parse_args()
+
+
+def main():
+    """
+    Main function to parse arguments, set variables, and run the Ansible playbook.
+    """
+    args = parse_args()
+
+    variables = set_variables(
+        args.username,
+        args.server_ip,
+        args.wg_password,
+        args.data_dir,
+        args.tcp_port,
+        args.udp_port,
+        args.ssh_key_path,
+    )
+
+    run_ansible_playbook(variables)
+
+
+if __name__ == '__main__':
+    main()
